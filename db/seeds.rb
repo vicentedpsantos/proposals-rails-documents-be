92fbc0ba29e7f1239555b6b6da2195793a336539
Vicente Santos <vicentedpsantos@gmail.com>
2019-03-25 01:45:46 -0300

    Sorted documents by descending sequence

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,9 @@
 
 10.times do |_d|
   FreelanceDocument.create!(
-    title: "Document #{_d}",
+    title: "Document #{_d + 1}",
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
     file_url: 'https://docs.google.com/document/d/1nW7r3Ko4D4uzSTbEW2i8BhFCb0X-zD6arovvD_LyorA/edit?usp=sharing',
-    image_url: 'https://coresites-cdn.factorymedia.com/mpora_new/wp-content/uploads/2016/02/Josie-Millard2.jpg'
+    image_url: 'https://coverfiles.alphacoders.com/508/50802.jpg'
   )
 end
